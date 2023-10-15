37010bd53cd5fe53429248d547e51381a02bee87
Debmalya Jash <gkedeb@gmail.com>
2023-10-14 20:16:04 -0400

    Painting the walls

---
+++ src/main/java/hard/PaintingTheWalls.java
@@ -0,0 +1,20 @@
+package hard;
+
+public class PaintingTheWalls {
+    /**
+     * You are given two 0-indexed integer arrays, cost and time, of size n representing the costs and the time taken
+     * to paint n different walls respectively. There are two painters available:
+     * <p>
+     * A paid painter that paints the ith wall in time[i] units of time and takes cost[i] units of money.
+     * <p>
+     * A free painter that paints any wall in 1 unit of time at a cost of 0.
+     * But the free painter can only be used if the paid painter is already occupied.
+     *
+     * @param cost 0-indexed integer array.
+     * @param time 0-indexed integer array.
+     * @return the minimum amount of money required to paint the n walls.
+     */
+    public int paintWalls(int[] cost, int[] time) {
+
+    }
+}
