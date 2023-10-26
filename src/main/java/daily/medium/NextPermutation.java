4987d9c64a6129ac625993ebfc4ddd1e2e2e9ebf
Debmalya Jash <gkedeb@gmail.com>
2023-10-26 17:06:55 -0400

    Create NextPermutation.java

---
+++ src/main/java/daily/medium/NextPermutation.java
@@ -0,0 +1,24 @@
+package daily.medium;
+
+public class NextPermutation {
+    /**
+     * A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+     * <p>
+     * For example, for arr = [1,2,3], the following are all the permutations of arr:
+     * [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+     * <p>
+     * The next permutation of an array of integers is the next lexicographically greater permutation of its integer.
+     * More formally, if all the permutations of the array are sorted in one container according to their lexicographical order,
+     * then the next permutation of that array is the permutation that follows it in the sorted container.
+     * If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+     * <p>
+     * For example, the next permutation of arr = [1,2,3] is [1,3,2].
+     * <p>
+     * Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+     *
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+
+    }
+}
