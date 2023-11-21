de63e9cea19ca8caba27063bed1f368fd51253f0
Debmalya Jash <gkedeb@gmail.com>
2023-11-20 19:09:48 -0500

    Create MinimumTimeToCollectGarbage.java

---
+++ src/main/java/daily/medium/MinimumTimeToCollectGarbage.java
@@ -0,0 +1,15 @@
+package daily.medium;
+
+public class MinimumTimeToCollectGarbage {
+    /**
+     * @param garbage 0-indexed array of strings garbage where garbage[i] represents the assortment of garbage at the ith house.
+     *                garbage[i] consists only of the characters 'M', 'P' and 'G' representing one unit of metal,
+     *                paper and glass garbage respectively. Picking up one unit of any type of garbage takes 1 minute.
+     * @param travel  0-indexed integer array travel where travel[i] is the number of minutes needed to go from house i to house i + 1.
+     * @return
+     */
+
+    public int garbageCollection(String[] garbage, int[] travel) {
+       return 0;
+    }
+}
