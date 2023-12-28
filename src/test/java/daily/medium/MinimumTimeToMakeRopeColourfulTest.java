74aa260ad16a81972ee9f2cb608cc3e3d58446ee
Debmalya Jash <gkedeb@gmail.com>
2023-12-28 09:05:47 -0500

    Minimum time to make rope colorful

--- src/test/java/daily/medium/MinimumTimeToMakeRopeColourfulTest.java
+++ src/test/java/daily/medium/MinimumTimeToMakeRopeColourfulTest.java
@@ -25,6 +25,18 @@ class MinimumTimeToMakeRopeColourfulTest {
 
     @Test
     void actual() {
+        assertEquals(3, minimumTimeToMakeRopeColourful.minCost1("abaac", new int[]{1, 2, 3, 4, 5}));
+        assertEquals(0, minimumTimeToMakeRopeColourful.minCost1("abc", new int[]{1, 2, 3}));
+        assertEquals(2, minimumTimeToMakeRopeColourful.minCost1("aabaa", new int[]{1, 2, 3, 4, 1}));
+        assertEquals(3, minimumTimeToMakeRopeColourful.minCost1("aaabaa", new int[]{1, 2, 1, 3, 4, 1}));
+        assertEquals(3, minimumTimeToMakeRopeColourful.minCost1("aaabaa", new int[]{1, 1, 1, 3, 4, 1}));
+        assertEquals(3, minimumTimeToMakeRopeColourful.minCost1("aaabaa", new int[]{2, 1, 1, 3, 4, 1}));
+        assertEquals(4, minimumTimeToMakeRopeColourful.minCost1("aaabaa", new int[]{1, 3, 2, 3, 4, 1}));
+        assertEquals(26, minimumTimeToMakeRopeColourful.minCost1("aaabbbabbbb", new int[]{3, 5, 10, 7, 5, 3, 5, 5, 4, 8, 1}));
+    }
+
+    @Test
+    void better() {
         assertEquals(3, minimumTimeToMakeRopeColourful.minCost("abaac", new int[]{1, 2, 3, 4, 5}));
         assertEquals(0, minimumTimeToMakeRopeColourful.minCost("abc", new int[]{1, 2, 3}));
         assertEquals(2, minimumTimeToMakeRopeColourful.minCost("aabaa", new int[]{1, 2, 3, 4, 1}));
