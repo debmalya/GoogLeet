244d4314cb4e1934ea13d077c3fb8e32a16b8d08
Debmalya Jash <gkedeb@gmail.com>
2025-02-11 21:24:12 -0500

    WorldClock

--- src/main/java/dream/WorldClock.java
+++ src/main/java/dream/WorldClock.java
@@ -3,13 +3,17 @@ package dream;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class WorldClock {
   public Map<String, String> getWorldClocks() {
-    Map<String, String> worldClockMap = new HashMap<>();
     DateTimeFormatter formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+    // Use TreeMap with custom comparator to sort by time
+    Map<String, String> worldClockMap =
+        new TreeMap<>(
+            Comparator.comparing(zoneId -> ZonedDateTime.now(ZoneId.of(zoneId)).format(formatter)));
     // Get all available timezone IDs
     for (String zoneId : ZoneId.getAvailableZoneIds()) {
       ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of(zoneId));
@@ -17,5 +21,11 @@ public class WorldClock {
       worldClockMap.put(zoneId, formattedTime);
     }
     return worldClockMap;
+  }
+
+  public static void main(String[] args) {
+    WorldClock worldClock = new WorldClock();
+    Map<String, String> worldClocks = worldClock.getWorldClocks();
+    worldClocks.forEach((zone, time) -> System.out.println(zone + ": " + time));
   }
 }
